7195a3fd65c54041b73c11ddf452056285b66efc
FcoHernan <FranciscoHRF@gmail.com>
2015-09-10 22:50:36 -0300

    mod mysql, connection and select

--- Js/app.js
+++ Js/app.js
@@ -1,5 +1,39 @@
-var request = require('request'),
-cheerio = require('cheerio');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'rsu2015',
+	database: 'WebScrap'
+});
+
+//Conocer si existe conexión
+connection.connect(function(error){
+	if(error){
+		throw error;
+	}else{
+		console.log('Conexion correcta.');
+	}
+});
+
+//Seleccionar páginas
+var query = connection.query('SELECT name, site FROM newspapers WHERE enabled="1"', function(error, result){
+	if(error){
+		throw error;
+	}else{
+		if(result.length > 0){
+			console.log(result);
+			console.log('\nSe encontraron '+result.length+' resultados\n');
+		}else{
+			console.log('Registro no encontrado');
+		}
+	}
+	}
+);
+
+
 
 request({url: 'http://thehackernews.com/', encoding: 'utf8'}, function(err, resp, body){
 	if(!err && resp.statusCode == 200){
@@ -11,3 +45,7 @@ request({url: 'http://thehackernews.com/', encoding: 'utf8'}, function(err, resp
 										});
 	}
 });
+
+
+
+connection.end();
